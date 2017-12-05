f33dc594b7354c16b6b78c2105d6372f5a9f437d
Heagan <georgesferopoulos@yahoo.com>
2017-12-05 11:23:48 +0200

    Update add.sh

--- add.sh
+++ add.sh
@@ -1,19 +1,20 @@
 lnk="http://127.0.0.1:8080/"
-lnk+=$1
-dir="/goinfre/gsferopo/mamp/apache2/htdocs/"
+dir=$HOME"/mamp/apache2/htdocs/"
 dir+=$2/
 ploc=$( echo | pwd | rev | cut -d / -f 1 | rev )
 loc=$( pwd )
-if [ ! -d $dir ] && [ -a $1 ] && [ $1 ] ; then
+
+if [ ! -d $dir ] && [ -a $1 ] && [ $1 ] || [ $@ > 2 ]; then
 	mkdir $dir
-	cp -r $loc $dir
-	echo $lnk
-	open $dir$ploc/$1
+	cp -fr $loc $dir
+	lnk+=$2/$ploc/$1
+	open $lnk
 	exit
 fi;
 if [ -d $dir ] && [ -a $1 ] && [ $1 ] ; then
-	cp -r $loc $dir
-	open $dir$ploc/$1
+	cp -fr $loc $dir
+	lnk+=$ploc/$1
+	open $lnk
 	exit
 fi;
 open $dir
